2a4b96703b57a2ef306c379eba59164402b921bf
AmishKakka <amishkakka@gmail.com>
2025-12-26 20:23:55 -0700

    Added correct options to select from, updated table layout.

--- backend/main.js
+++ backend/main.js
@@ -3,9 +3,21 @@
 async function runSimulation() {
     const numInput = document.getElementById('num_requests').value;
     const count = parseInt(numInput);
-    
     // Replace with your real Lambda API URL
     const apiUrl = "";
+
+    if (count > 50) {
+        alert("Please enter a number less than or equal to 50.");
+        return;
+    }
+    if (count < 1) {
+        alert("Please enter a number greater than or equal to 1.");
+        return;
+    }
+
+    // Set the requests count
+    document.getElementById('r1_requests').innerHTML = `${count}`;
+    document.getElementById('r2_requests').innerHTML = `${count}`;
 
     try {
         const response = await fetch(apiUrl, {
@@ -17,8 +29,14 @@ async function runSimulation() {
         const data = await response.json();
         console.log("Simulation Result:", data);
         // alert(data.message);
-        
     } catch (error) {
         console.error("Error:", error);
+    }
+
+    // Live update the incoming requests
+    for (let i = 1; i <= count; i++) {
+        document.getElementById('r1_incoming_requests').innerHTML = `${i}`;
+        document.getElementById('r2_incoming_requests').innerHTML = `${i}`;
+        await new Promise(resolve => setTimeout(resolve, 500));
     }
 }
