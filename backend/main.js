00203fc2ccf92c4b99edc28d98aa2d555a9ae01c
AmishKakka <amishkakka@gmail.com>
2025-12-28 00:04:56 -0700

    Added function to query an image from S3

--- backend/main.js
+++ backend/main.js
@@ -1,11 +1,14 @@
 // Handle requests and communicate with Lambda functions
 
+// Replace with your real Lambda API URL
+const apiUrl = "";
+
+
 async function runSimulation() {
     const numInput = document.getElementById('num_requests').value;
     const count = parseInt(numInput);
-    // Replace with your real Lambda API URL
-    const apiUrl = "";
-
+    
+    // Input validation
     if (count > 50) {
         alert("Please enter a number less than or equal to 50.");
         return;
@@ -19,18 +22,23 @@ async function runSimulation() {
     document.getElementById('r1_requests').innerHTML = `${count}`;
     document.getElementById('r2_requests').innerHTML = `${count}`;
 
+    // Sending query to 'simulate-s3-hits' Lambda function
     try {
         const response = await fetch(apiUrl, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ num: count })
+            body: JSON.stringify({ 
+                num: count,
+                mode: "simulation"
+             })
         });
         
         const data = await response.json();
         console.log("Simulation Result:", data);
-        // alert(data.message);
+        alert(data.message);
     } catch (error) {
         console.error("Error:", error);
+        alert(error);
     }
 
     // Live update the incoming requests
@@ -38,5 +46,31 @@ async function runSimulation() {
         document.getElementById('r1_incoming_requests').innerHTML = `${i}`;
         document.getElementById('r2_incoming_requests').innerHTML = `${i}`;
         await new Promise(resolve => setTimeout(resolve, 500));
+    }
+}
+
+
+async function runQuery() {
+    targetQuery = document.getElementById('target').value;
+    numResults = document.getElementById('n_output').value;
+    console.log(`Querying for ${targetQuery} with ${numResults} results.`);
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 
+                target: targetQuery,
+                num: numResults,
+                mode: "query"
+             })
+        });
+        
+        const data = await response.json();
+        console.log("Query Result:", data);
+        alert(data.message);
+    } catch (error) {
+        console.error("Error:", error);
+        alert(error);
     }
 }
