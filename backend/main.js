55a8c9fcb718f5ec4824eb46ea7fcbb767926c03
AmishKakka <amishkakka@gmail.com>
2026-01-01 11:34:04 -0700

    Updated the UI and added flashcards

--- backend/main.js
+++ backend/main.js
@@ -4,19 +4,50 @@
 const apiUrl = "";
 
 
+const flashcards = [
+    "This simulation sends requests to two different AWS regions (us-east-2 and us-west-1). You select the maximum number of requests to send (between 10 and 200) to each region.",
+    "Route 53 uses Latency-Based Routing to direct user traffic. It automatically routes you to the AWS region that provides the fastest response time.",
+    "If one region goes offline, Route 53 Health Checks will detect the failure and automatically redirect all traffic to the healthy region, ensuring zero downtime.",
+    "Both regions are Active-Active. This means they both handle traffic simultaneously, unlike a disaster recovery setup where one sits idle."
+];
+
+let currentCardIndex = 0;
+
+function updateCard() {
+    const textElement = document.getElementById('flashcard-text');
+    textElement.style.opacity = 0; 
+    
+    setTimeout(() => {
+        textElement.textContent = flashcards[currentCardIndex];
+        textElement.style.opacity = 1; 
+    }, 100);
+}
+
+function nextCard() {
+    currentCardIndex = (currentCardIndex + 1) % flashcards.length;
+    updateCard();
+}
+
+function prevCard() {
+    currentCardIndex = (currentCardIndex - 1 + flashcards.length) % flashcards.length;
+    updateCard();
+}
+
+
 async function runSimulation() {
     document.getElementById('simulate-button').innerHTML = "Simulating...";
     document.getElementById('simulate-button').disabled = true;
+    const stopButton = document.getElementById('stop-button');
     const numInput = document.getElementById('num_requests').value;
     const count = parseInt(numInput);
     
     // Input validation
-    if (count > 50) {
-        alert("Please enter a number less than or equal to 50.");
+    if (count > 200) {
+        alert("Please enter a number less than or equal to 200.");
         return;
     }
-    if (count < 1) {
-        alert("Please enter a number greater than or equal to 1.");
+    if (count < 10) {
+        alert("Please enter a number greater than or equal to 10.");
         return;
     }
 
@@ -35,10 +66,15 @@ async function runSimulation() {
              })
         });
         
+        stopButton.disabled = false;
         const data = await response.json();
-        console.log("Simulation Result:", data);
+        while (stopButton.click()) {
+            console.log("Simulation Result:", data);
+        }
+        
         document.getElementById('simulate-button').disabled = false;
         document.getElementById('simulate-button').innerHTML = "Start";
+        stopButton.disabled = true;
         alert(data.message);
     } catch (error) {
         console.error("Error:", error);
@@ -55,17 +91,26 @@ async function runSimulation() {
 
 
 async function runQuery() {
-    document.getElementById('search-button').innerHTML = "Searching...";
-    document.getElementById('search-button').disabled = true;
     const listContainer = document.getElementById('image-list');
 
     targetQuery = document.getElementById('target').value;
     numResults = document.getElementById('n_output').value;
+    // Input validation
+    if (numResults > 200) {
+        alert("Please enter a number less than or equal to 200.");
+        return;
+    }
+    if (numResults < 1) {
+        alert("Please enter a number greater than or equal to 1.");
+        return;
+    }
     if (targetQuery === "") {
         alert("Please enter a valid target query.");
         document.getElementById('search-button').innerHTML = "Search";
         return;
     }
+    document.getElementById('search-button').innerHTML = "Searching...";
+    document.getElementById('search-button').disabled = true;
     console.log(`Querying for ${targetQuery} with ${numResults} results.`);
 
     try {
@@ -84,6 +129,7 @@ async function runQuery() {
         const data = await response.json();
         document.getElementById('search-button').disabled = false;
         document.getElementById('search-button').innerHTML = "Search";
+        document.getElementById('search-latency').innerHTML = `Latency: ${Math.trunc(data.latency*1000)}ms`;
         console.log("Query Result:", data);
 
         // Display image files and their respective URLs
